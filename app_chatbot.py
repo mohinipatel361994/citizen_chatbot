6c1b0f7aad9259545c400d1a16e507760e6dbb99
mohinipatel361994 <mohinipatel361994@gmail.com>
2025-04-09 10:59:13 +0530

    Update app_chatbot.py

--- app_chatbot.py
+++ app_chatbot.py
@@ -230,7 +230,7 @@ def load_faiss_vectorstore():
         st.error("FAISS index not found. Please rebuild the FAISS index using the correct embedding model.")
         logging.error("FAISS index not found in expected directory.")
         return None
-    model_name = "all-MiniLM-L6-v2"
+    model_name = "sentence-transformers/paraphrase-xlm-r-multilingual-v1"
     embeddings = HuggingFaceEmbeddings(model_name=model_name)
     expected_dim = len(embeddings.embed_query("test query"))
     
